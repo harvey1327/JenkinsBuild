38c5f35bf23f7c90aaf539ea758e3ebf87e5bfca
Harvey Bal <harvey.bal@paconsulting.com>
2019-01-21 12:34:41 +0000

    Moved multibranch_pipeline.groovy script as scriptText within seed job

--- groovy_scripts/seed_job.groovy
+++ groovy_scripts/seed_job.groovy
@@ -13,8 +13,33 @@ builder.setIgnoreExisting(false)
 builder.setRemovedConfigFilesAction(javaposse.jobdsl.plugin.RemovedConfigFilesAction.DELETE)
 builder.setRemovedJobAction(javaposse.jobdsl.plugin.RemovedJobAction.DELETE)
 builder.setRemovedViewAction(javaposse.jobdsl.plugin.RemovedViewAction.DELETE)
-builder.setScriptText('$JENKINS_HOME/init.groovy.d/multibranch_pipeline.groovy')
+builder.setScriptText(getScriptText())
 job.buildersList.add(builder)
 
 job.save()
 instance.save()
+
+def getScriptText() {
+  return '''
+  def gitArray = ['https://github.com/harvey1327/KotlinMicroService.git']
+
+  gitArray.each { gitAddress ->
+    setupPipelineJob(gitAddress)
+  }
+
+  def setupPipelineJob(String gitAddress) {
+    def jobName = gitAddress.substring(gitAddress.lastIndexOf("/")+1,gitAddress.lastIndexOf("."))
+    multibranchPipelineJob("$jobName"){
+      branchSources {
+          git {
+              remote(gitAddress)
+              includes('*')
+          }
+      }
+      triggers {
+        periodic(5)
+      }
+    }
+  }
+  '''
+}

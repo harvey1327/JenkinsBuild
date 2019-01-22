9f2d4aa6f5f9f1885198b37b79c9f60934a670d3
Harvey Bal <harvey.bal@paconsulting.com>
2019-01-22 12:20:01 +0000

    Changed intitial seed job

--- groovy_scripts/seed_job.groovy
+++ groovy_scripts/seed_job.groovy
@@ -8,49 +8,16 @@ def job = instance.createProject(FreeStyleProject, 'Seed-Job')
 job.setDisplayName('Seed-Job')
 job.addTrigger(new TimerTrigger('H/15 * * * *'))
 
+def scm = new hudson.plugins.git.GitSCM('https://github.com/harvey1327/JenkinsBuild')
+job.setScm(scm)
+
 def builder = new javaposse.jobdsl.plugin.ExecuteDslScripts()
 builder.setIgnoreExisting(false)
 builder.setRemovedConfigFilesAction(javaposse.jobdsl.plugin.RemovedConfigFilesAction.DELETE)
 builder.setRemovedJobAction(javaposse.jobdsl.plugin.RemovedJobAction.DELETE)
 builder.setRemovedViewAction(javaposse.jobdsl.plugin.RemovedViewAction.DELETE)
-builder.setScriptText(getScriptText())
+builder.setTargets('**/job_dsl/*.groovy')
 job.buildersList.add(builder)
 
 job.save()
 instance.save()
-
-def getScriptText() {
-  return '''
-  def gitArray = ['https://github.com/harvey1327/KotlinMicroService.git']
-
-  gitArray.each { gitAddress ->
-    setupPipelineJob(gitAddress)
-  }
-
-  createBuildMonitorView()  
-
-  def createBuildMonitorView(){
-    buildMonitorView('Overall View'){
-      jobs {
-        regex(".*(?<!Seed-Job)$")
-      }
-      recurse(true)
-    }
-  }
-
-  def setupPipelineJob(String gitAddress) {
-    def jobName = gitAddress.substring(gitAddress.lastIndexOf("/")+1,gitAddress.lastIndexOf("."))
-    multibranchPipelineJob("$jobName"){
-      branchSources {
-          git {
-              remote(gitAddress)
-              includes('*')
-          }
-      }
-      triggers {
-        periodic(5)
-      }
-    }
-  }
-  '''
-}

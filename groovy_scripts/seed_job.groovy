f15f4d869a834b63ebc6a427654533f1d196729b
Harvey Bal <harvey.bal@paconsulting.com>
2019-01-22 14:15:23 +0000

    Refactored seedjob

--- groovy_scripts/seed_job.groovy
+++ groovy_scripts/seed_job.groovy
@@ -1,21 +1,28 @@
 import jenkins.model.Jenkins;
 import hudson.model.FreeStyleProject;
 import hudson.triggers.*;
+import hudson.plugins.git.*;
+import javaposse.jobdsl.plugin.*;
 
 def instance = Jenkins.getInstance()
 
 def job = instance.createProject(FreeStyleProject, 'Seed-Job')
 job.setDisplayName('Seed-Job')
+
+// Uses hudson.triggers.*
 job.addTrigger(new TimerTrigger('H/15 * * * *'))
 
-def scm = new hudson.plugins.git.GitSCM('https://github.com/harvey1327/JenkinsBuild')
+// Uses hudson.plugins.git.*
+def scm = new GitSCM('https://github.com/harvey1327/JenkinsBuild')
+scm.branches = [new BranchSpec("*/master")];
 job.setScm(scm)
 
-def builder = new javaposse.jobdsl.plugin.ExecuteDslScripts()
+// Uses javaposse.jobdsl.plugin.*
+def builder = new ExecuteDslScripts()
 builder.setIgnoreExisting(false)
-builder.setRemovedConfigFilesAction(javaposse.jobdsl.plugin.RemovedConfigFilesAction.DELETE)
-builder.setRemovedJobAction(javaposse.jobdsl.plugin.RemovedJobAction.DELETE)
-builder.setRemovedViewAction(javaposse.jobdsl.plugin.RemovedViewAction.DELETE)
+builder.setRemovedConfigFilesAction(RemovedConfigFilesAction.DELETE)
+builder.setRemovedJobAction(RemovedJobAction.DELETE)
+builder.setRemovedViewAction(RemovedViewAction.DELETE)
 builder.setTargets('**/job_dsl/*.groovy')
 job.buildersList.add(builder)
 

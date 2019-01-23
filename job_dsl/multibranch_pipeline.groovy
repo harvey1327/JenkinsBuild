da3c42de6c9ab06a6d6665869dc807f4f95086cd
Harvey Bal <harvey.bal@paconsulting.com>
2019-01-23 16:54:02 +0000

    Moved boring seed job to exciting multistage pipeline

--- job_dsl/multibranch_pipeline.groovy
+++ job_dsl/multibranch_pipeline.groovy
@@ -1,23 +1,26 @@
 def gitArray = ['https://github.com/harvey1327/KotlinMicroService.git']
 def groupName = 'service'
 
-gitArray.each { gitAddress ->
-  setupPipelineJob(gitAddress)
-}
+node {
 
-createBuildMonitorView()
-
-def createBuildMonitorView(){
-  buildMonitorView("$groupName View"){
-    jobs {
-      regex("$groupName-.*")
+  gitArray.each { gitAddress ->
+    def jobName = gitAddress.substring(gitAddress.lastIndexOf("/")+1,gitAddress.lastIndexOf("."))
+    stage("Set up Multibranch for $jobName"){
+      setupPipelineJob(gitAddress, jobName)
     }
-    recurse(true)
+  }
+
+  stage('Build Monitor View'){
+    buildMonitorView("$groupName View"){
+      jobs {
+        regex("$groupName-.*")
+      }
+      recurse(true)
+    }
   }
 }
 
-def setupPipelineJob(String gitAddress) {
-  def jobName = gitAddress.substring(gitAddress.lastIndexOf("/")+1,gitAddress.lastIndexOf("."))
+def setupPipelineJob(String gitAddress, String jobName) {
   multibranchPipelineJob("$groupName-$jobName"){
     branchSources {
         git {
